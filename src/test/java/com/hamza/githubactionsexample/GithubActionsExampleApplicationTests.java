bb3e4cf7f1c0a78aee08ab97db35634b95f11a1c
hamzachikar <chikar.h@outlook.fr>
2021-02-17 11:18:44 +0100

    init

---
+++ src/test/java/com/hamza/githubactionsexample/GithubActionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.githubactionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
